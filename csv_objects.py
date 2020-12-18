27d314ee0dfa8581c85f18d218aaaff3a3246991
Denis Seleznyov <selden.new@gmail.com>
2020-12-18 17:05:38 +0500

    замена ссылок

--- csv_objects.py
+++ csv_objects.py
@@ -53,60 +53,32 @@ class News:
                         files.append(file)
                         filenames.append(file.encoded_filename)
                         # TODO разобраться
-                        self.a_body = str(self.a_body).replace(file.file_full_path, file.str_new_link)     # Замены ссылки
+                        #self.a_body = str(self.a_body).replace(file.file_full_path, file.str_new_link)     # Замены ссылки
                         self.a_body = str(self.a_body).replace(file.file_full_path, file.new_link)
             # TODO сделать нормальную обработку
             except Exception as e:
                 print(e, 'test')
         return files, filenames
 
-    # TODO подумать как можно объединить общий функционал на функции обработки файлов
-    # Получение файла Новостей из описания Новостей
-    def get_file_from_body(config, obj):
-        old_sitename = config["old_name"]
-        pattern_file_1 = fr'(<a href=\"((?:http:\/\/(?:www\.|){old_sitename}|)\/(((?:dokumentydok\/[^\/]{{1,75}}|upload\/iblock\/[^\/]{{0,4}}|Upload\/files|Files\/DiskFile\/(?:|[0-9]{{4}}\/)[a-zA-Z]{{1,10}}|opendata|Storage\/Image\/PublicationItem\/Image\/src\/[0-9]{{1,5}})\/)([^>]{{1,450}}\.[a-zA-Z]{{3,5}})))\s?\"[^>]{{0,250}}>)'
-        pattern_file_2 = fr'(<img alt=\"[^\/]{{0,50}}\"(?:\sclass=\"[^\/]{{0,50}}\"|)\ssrc=\"((?:http:\/\/(?:www\.|){old_sitename}|)\/(((?:Upload\/images\/|Storage\/Image\/PublicationItem\/Article\/src\/[0-9]{{1,5}}\/))([^>\/]{{1,450}}\.[a-zA-Z]{{3,5}})))\"[^>]{{0,550}}>)'
-        # pattern_file_2 = fr'(<img alt=\".{0,50}\" )'
-        pattern_list = {
-            "news":     pattern_file_1,         # паттерн 1
-            "news2":    pattern_file_2,         # паттерн 2
-        }
-        files = []
-        filenames = []
-        for link_type, pattern in pattern_list.items():
-            # print(pattern)
-            try:
-                links = re.findall(pattern, obj.a_body)
-                # Если есть совпадения
-                if len(links) > 0:
-                    for link in links:
-                        data = {
-                            "full_link":            link[0],    # Полная ссылка с a href и стилями. Пример:     <a href="http://szn74.ru/Upload/files/Приказ МПР 496 от 111113 с изм 141217.rtf">
-                            "file_full_path":       link[1],    # Ссылка на файл.                   Пример:     http://szn74.ru/Upload/files/Приказ МПР 496 от 111113 с изм 141217.rtf
-                            "file_path":            link[2],    # Полный путь до файла.             Пример:     Upload/files/Приказ МПР 496 от 111113 с изм 141217.rtf
-                            "file_relative_path":   link[3],    # Папка файла.                      Пример:     Upload/files/
-                            "file":                 link[4],    # Имя файла с расширением.          Пример:     Приказ МПР 496 от 111113 с изм 141217.rtf
-                        }
-                        file = NewsFile(config, data)
-                        files.append(file)
-                        filenames.append(file.encoded_filename)
-                        old_file_path = file.file_full_path
-                        new_file_path = file.str_new_link
-                        obj.a_body = str(obj.a_body).replace(old_file_path, new_file_path)     # Замены ссылки
-            except Exception as e:
-                print(e)
-        return files, filenames
-
     def delete_links(self):
         # TODO сделать передачу имени в регулярку
         old_sitename = self.config["old_name"]
-        pattern_npa = r'(<a href=\"((?:http:\/\/(?:ruk\.|)pravmin74.ru|)(\/normativnye-pravovye-akty\/[^\/]{1,250}\/[^\/\"]{1,250}))\"[^>]{0,100}>)'
-        pattern_news = r'(<a href=\"((?:http:\/\/(?:ruk\.|)pravmin74.ru|)(\/novosti\/[^\/\"]{1,250}))\"[^>]{0,100}>)'
-        pattern_page = r'(<a href=\"((?:http:\/\/(?:ruk\.|)pravmin74.ru|)(\/[^\/\"]{1,100}))\"[^>]{0,100}>)'
+        genum_pattern_page = fr'(<a href=\"((?:http:\/\/(?:www\.|){old_sitename}|)\/(htmlpages\/(?:Show|CmsHtmlPageList)\/[^\"]{{1,75}}(?:\/[^\"]{{1,75}}\/[^\"]{{1,75}}|\/[^\"]{{1,75}}|)))\"[^>]{{0,250}}>)'
+        genum_pattern_npa = fr'(<a href=\"((?:http:\/\/(?:www\.|){old_sitename}|)\/(LegalActs\/Show\/[^\/>]{{1,10}}))\"[^>]{{0,250}}>)'
+        genum_pattern_single_page = fr'(<a href=\"((?:http:\/\/(?:www\.|){old_sitename}|)(?:|\/|\/(?:InternetReception|LegalActs)))\"[^>]{{0,250}}>)'
+        genum_pattern_id = fr'(<a href=\"((?:http:\/\/(?:www\.|){old_sitename}|)\/Publications\/[a-zA-Z]{{1,15}}(?:\/Show\?id=[0-9]{{0,10}}|))\"[^>]{{0,250}}>)'
+        sinta_pattern_npa = r'(<a href=\"((?:http:\/\/(?:ruk\.|)pravmin74.ru|)(\/normativnye-pravovye-akty\/[^\/]{1,250}\/[^\/\"]{1,250}))\"[^>]{0,100}>)'
+        sinta_pattern_news = r'(<a href=\"((?:http:\/\/(?:ruk\.|)pravmin74.ru|)(\/novosti\/[^\/\"]{1,250}))\"[^>]{0,100}>)'
+        sinta_pattern_page = r'(<a href=\"((?:http:\/\/(?:ruk\.|)pravmin74.ru|)(\/[^\/\"]{1,100}))\"[^>]{0,100}>)'
+
         pattern_list = {
-            "npa_link":         pattern_npa,           # паттерн для поиска ссылок на страницы
-            "news_link":        pattern_news,           # паттерн для поиска ссылок на страницы
-            "page_link":        pattern_page,           # паттерн для поиска ссылок на страницы
+            "genum_page":           genum_pattern_page,         # genum паттерн для поиска ссылок на страницы
+            "genum_npa":            genum_pattern_npa,          # genum паттерн для поиска ссылок на НПА
+            "genum_single_page":    genum_pattern_single_page,  # genum паттерн для поиска ссылок на приемнуюкорневые страницы
+            "genum_news":           genum_pattern_id,           # genum паттерн для поиска ссылок на новости
+            "sinta_npa":            sinta_pattern_npa,          # sinta паттерн для поиска ссылок на НПА
+            "sinta_news":           sinta_pattern_news,         # sinta паттерн для поиска ссылок на новости
+            "sinta_page":           sinta_pattern_page,         # sinta паттерн для поиска ссылок на страницы
         }
         for link_type, pattern in pattern_list.items():
             try:
@@ -189,37 +161,6 @@ class News:
     #         except Exception as e:
     #             print(e)
 
-    # # TODO Разобраться
-    # def make_mediafiles(self):
-    #     image_list = []
-    #     image_name_list = []
-    #     # fromid = self.a_ouid
-    #     description = ""
-    #     for pattern in self.pattern_list:
-    #         try:
-    #             pattern_image_list = re.findall(pattern, self.a_body)
-    #             # Если есть совпадения
-    #             if len(pattern_image_list) > 0:
-    #                 for temp_image in pattern_image_list:
-    #                     # Проверка если кортеж - то значит 2м объектом идет путь до картинки
-    #                     if type(temp_image) == tuple and temp_image[1] != 'www.':
-    #                         # if type(temp_image) == tuple:
-    #                         # print(type(temp_image))
-    #                         toid = str(temp_image[1])
-    #                         image = NewsFile(toid, description, fromid, self.folder_name)
-    #                         image_name_list.append(str(temp_image[1]).replace("/Upload/images/", "").replace("/Storage/Image/PublicationItem/Article/src/", ""))
-    #                         image_list.append(image)
-    #         except Exception as e:
-    #             print(e)
-    #     return image_list, image_name_list
-
-    # # TODO Разобраться
-    # def get_news_data(self):
-    #     data = (
-    #         self.a_title, self.a_date, self.a_image_index, self.a_body,
-    #         self.a_publ_date, self.a_resume, self.a_structure
-    #     )
-    #     return data
 
     # # TODO Разобраться
     # def delete_links(self):
