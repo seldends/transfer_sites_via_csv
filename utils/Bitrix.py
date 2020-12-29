745eb99e3304fcfdb289e45c23a40057f2abc54f
Denis Seleznyov <selden.new@gmail.com>
2020-12-29 20:24:10 +0500

    рефакторинг функций в бд битрикс

--- utils/Bitrix.py
+++ utils/Bitrix.py
@@ -21,128 +21,62 @@ class DatabaseBitrix(Database):
                 finally:
                     print('MariaDB Connection opened successfully.')
 
-        # Функция для получения новостей
-    def get_news_list(self, params):
+    def get_obj_list(self, params):
         # MariaDB
-        select_news_local = '''
+        select_obj_local = '''
             SELECT
             ID as id,
             IBLOCK_ID,
             NAME as title,
             DATE_CREATE as date_create,
-            PREVIEW_PICTURE as index_img,
             DETAIL_TEXT as body,
             TIMESTAMP_X as date_publ,
             PREVIEW_TEXT as resume,
-            XML_ID
-            FROM imchel_10_12.b_iblock_element
-            -- WHERE DATE_CREATE > '2018-01-01 00:00:00'
-            WHERE IBLOCK_ID = 152
-            -- AND IBLOCK_ID = 13
-            AND ACTIVE = 'Y'
-            order by ID DESC
-            ;
-            '''
-        # news_list = self.select_rows(select_news_local, params)
-        news_list = self.select_rows(select_news_local)
-        return news_list
-
-        # Функция для получения новостей
-    def get_npa_list(self, params):
-        # MariaDB
-        select_npa_local = '''
-            SELECT
-            ID as id,
-            IBLOCK_ID,
-            NAME as title,
-            DATE_CREATE as date_create,
-            DETAIL_TEXT as body,
-            TIMESTAMP_X as date_publ,
-            XML_ID
+            PREVIEW_PICTURE as index_img
             FROM imchel_10_12.b_iblock_element
             WHERE ACTIVE = 'Y'
-            AND IBLOCK_ID in (145)
+            AND DATE_CREATE > '2018-01-01 00:00:00'
+            -- AND IBLOCK_ID in (13)
+            -- AND IBLOCK_ID in (153)
+            AND IBLOCK_ID in (39)
+            -- AND IBLOCK_ID in (13)
+            -- AND IBLOCK_ID in (13)
+            -- AND IBLOCK_ID in (13)
+            -- AND IBLOCK_ID in (13)
+            -- AND IBLOCK_ID in (13)
+            -- AND IBLOCK_ID in (13)
+            -- AND IBLOCK_ID in (13)
             order by ID DESC
             ;
             '''
         # news_list = self.select_rows(select_news_local, params)
-        npa_list = self.select_rows(select_npa_local)
-        return npa_list
+        obj_list = self.select_rows(select_obj_local)
+        return obj_list
 
-        # Функция для получения ouid нпа
-    def get_npa_files_list(self, id):
-        select_npafiles_local = '''
+    def get_obj_files_list(self, id):
+        select_obj_files_local = '''
             SELECT CONCAT('upload/',b_file.SUBDIR, '/', b_file.FILE_NAME) as file_path
             FROM imchel_10_12.b_file
             LEFT JOIN imchel_10_12.b_iblock_element_property
             ON b_iblock_element_property.VALUE = imchel_10_12.b_file.ID
             WHERE b_iblock_element_property.IBLOCK_ELEMENT_ID=%s;
             '''
-        npafiles_list = self.select_rows(select_npafiles_local, id)
+        obj_files_list = self.select_rows(select_obj_files_local, id)
+        return obj_files_list
+
+    # Функция для получения ouid нпа
+    def get_npa_files_list(self, id):
+        npafiles_list = self.get_obj_files_list(id)
         return npafiles_list
 
-    def get_auction_list(self, params):
-        # MariaDB
-        select_auction_local = '''
-            SELECT
-            ID as id,
-            IBLOCK_ID,
-            NAME as title,
-            DATE_CREATE as date_create,
-            DETAIL_TEXT as body,
-            TIMESTAMP_X as date_publ,
-            XML_ID
-            FROM imchel_10_12.b_iblock_element
-            WHERE ACTIVE = 'Y'
-            AND IBLOCK_ID in (102)
-            order by ID DESC
-            ;
-            '''
-        # news_list = self.select_rows(select_news_local, params)
-        auction_list = self.select_rows(select_auction_local)
-        return auction_list
+    def get_doc_files_list(self, id):
+        docfiles_list = self.get_obj_files_list(id)
+        return docfiles_list
 
-        # Функция для получения ouid нпа
     def get_auction_files_list(self, id):
-        select_auctionfiles_local = '''
-            SELECT CONCAT('upload/',b_file.SUBDIR, '/', b_file.FILE_NAME) as file_path
-            FROM imchel_10_12.b_file
-            LEFT JOIN imchel_10_12.b_iblock_element_property
-            ON b_iblock_element_property.VALUE = imchel_10_12.b_file.ID
-            WHERE b_iblock_element_property.IBLOCK_ELEMENT_ID=%s;
-            '''
-        auctionfiles_list = self.select_rows(select_auctionfiles_local, id)
+        auctionfiles_list = self.get_obj_files_list(id)
         return auctionfiles_list
 
-    def get_vacancy_list(self, params):
-        # MariaDB
-        select_vacancy_local = '''
-            SELECT
-            ID as id,
-            IBLOCK_ID,
-            NAME as title,
-            DATE_CREATE as date_create,
-            DETAIL_TEXT as body,
-            TIMESTAMP_X as date_publ,
-            XML_ID
-            FROM imchel_10_12.b_iblock_element
-            WHERE ACTIVE = 'Y'
-            AND IBLOCK_ID in (12)
-            order by ID DESC
-            ;
-            '''
-        # news_list = self.select_rows(select_news_local, params)
-        vacancy_list = self.select_rows(select_vacancy_local)
-        return vacancy_list
-
-        # Функция для получения ouid нпа
     def get_vacancy_files_list(self, id):
-        select_vacancyfiles_local = '''
-            SELECT CONCAT('upload/',b_file.SUBDIR, '/', b_file.FILE_NAME) as file_path
-            FROM imchel_10_12.b_file
-            LEFT JOIN imchel_10_12.b_iblock_element_property
-            ON b_iblock_element_property.VALUE = imchel_10_12.b_file.ID
-            WHERE b_iblock_element_property.IBLOCK_ELEMENT_ID=%s;
-            '''
-        vacancyfiles_list = self.select_rows(select_vacancyfiles_local, id)
+        vacancyfiles_list = self.get_obj_files_list(id)
         return vacancyfiles_list
